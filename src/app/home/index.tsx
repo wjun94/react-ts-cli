872b344c224ba986c432b4a7a805fe6a4c9e8eab
wjun94 <kasepa1994@gmail.com>
2020-11-08 22:40:25 +0800

    feat 添加redux

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { RouteComponentProps } from 'react-router'
+import store from '../../store/store'
 
-export default function Login(props: any) {
-    console.log(props)
-    function back() {
-        props.history.goBack()
+export default class Home extends React.Component<RouteComponentProps, {}> {
+    componentDidMount() {
+        store.subscribe(() => {
+            this.setState({})
+        })
     }
-    return (
-        <div>
-            <p>主页</p>
-            <button onClick={back}>返回</button>
+    back = () => {
+        this.props.history.goBack()
+    }
+
+    onSend = () => {
+        store.dispatch({
+            type: 'send_type',
+            value: '测试成功'
+        })
+    }
+
+    render() {
+        return <div>
+            <p>主页 {store.getState().value}</p>
+            <button onClick={this.back}>返回</button>
+            <button onClick={this.onSend}>传值</button>
         </div>
-    )
-}
+    }
+} 
