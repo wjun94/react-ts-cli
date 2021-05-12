c8ce9cf1ca42ef9448a2caca7cacd5f7e17d3efd
项文俊 <wj.xiang>
2021-05-12 15:47:38 +0800

    chore(redux): 添加redux-thunk中间件

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,12 @@
-import {
-    createStore,
-    combineReducers,
-} from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import homeReducer from './home/reducer'
 import detailsReducer from './home/reducer'
 
 export default createStore(
-    combineReducers({
-        ...homeReducer,
-        ...detailsReducer
-    }),
+  combineReducers({
+    ...homeReducer,
+    ...detailsReducer,
+  }),
+  applyMiddleware(thunk)
 )
